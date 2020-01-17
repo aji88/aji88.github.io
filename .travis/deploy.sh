2bc2131bdd425b7dc16e5b1bb43bc35594654977
aji88 <y_m_z@126.com>
2020-01-17 15:45:13 +0800

    x

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 # deploy
-openssl aes-256-cbc -K $encrypted_b7e9f3138b36_key -iv $encrypted_b7e9f3138b36_iv -in deploy_rsa.enc -out ~/.ssh/deploy_rsa -d
+openssl aes-256-cbc -K $encrypted_b7e9f3138b36_key -iv $encrypted_b7e9f3138b36_iv -in ./deploy_rsa.enc -out ~/.ssh/deploy_rsa -d
 chmod 600 ~/.ssh/deploy_rsa
 eval $(ssh-agent)
-ssh-add ~/.ssh/deploy_rsa
+ssh-add ~/.ssh/
+cp ./ssh_config ~/.ssh/config
 git config --global user.name "aji88"
 git config --global user.email "y_m_z@126.com"
 cd ../public
