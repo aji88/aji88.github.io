79c3a7c32a574d8fba0a143d5ea72621c36c4744
aji88 <y_m_z@126.com>
2020-01-19 10:09:49 +0800

    x

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -3,14 +3,14 @@
 openssl aes-256-cbc -K $encrypted_b7e9f3138b36_key -iv $encrypted_b7e9f3138b36_iv -in ./deploy_rsa.enc -out ~/.ssh/deploy_rsa -d
 chmod 600 ~/.ssh/deploy_rsa
 eval $(ssh-agent)
-ssh-add ~/.ssh/
+ssh-add ~/.ssh/deploy_rsa
 cp ./ssh_config ~/.ssh/config
 git config --global user.name "aji88"
 git config --global user.email "y_m_z@126.com"
-#cd ../public
-#git init
-#git config user.name "aji88"
-#git config user.email "y_m_z@126.com"
-#git add .
-#git commit -m "add/edit"
-#git push --force git@github.com:aji88/blog.git hexo:master
+cd ../public
+git init
+git config user.name "aji88"
+git config user.email "y_m_z@126.com"
+git add .
+git commit -m "add/edit"
+git push --force git@github.com:aji88/blog.git hexo:master
